2eb9b575cd8dc76e18507db6ae0105576e7138aa
JoonwooKim <kkn8010@gmail.com>
2022-12-18 20:25:32 +0900

    update: 회원가입 기능 필요한 내용 추가

--- src/main/java/traveler/travel/dto/UserDto.java
+++ src/main/java/traveler/travel/dto/UserDto.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import traveler.travel.enums.Authority;
+import traveler.travel.enums.Gender;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
@@ -20,5 +21,12 @@ public class UserDto {
     @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
     @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}", message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
     private String password;
+
+    private String phoneNum;
+    private String birth;
+    private String nickname;
+
+//    private Gender gender;
+
 
 }
