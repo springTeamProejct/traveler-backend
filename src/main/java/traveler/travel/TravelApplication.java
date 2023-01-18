2c44fc6786217ce9d4ba06c92bbbc75b3982e69f
syhwang4223 <syhwang4223@gmail.com>
2023-01-18 17:36:35 +0900

    etc : Querydsl 라이브러리 추가 버전 5.0.0

--- src/main/java/traveler/travel/TravelApplication.java
+++ src/main/java/traveler/travel/TravelApplication.java
@@ -1,8 +1,12 @@
 package traveler.travel;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import javax.persistence.EntityManager;
 
 @EnableJpaAuditing // JPA Auditing 활성화
 @SpringBootApplication
@@ -10,6 +14,11 @@ public class TravelApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TravelApplication.class, args);
+	}
+
+	@Bean
+	JPAQueryFactory jpaQueryFactory(EntityManager em) {
+		return new JPAQueryFactory(em);
 	}
 
 }
