ef286b4927cad91f2e62fab3569c48d017764d9b
syhwang4223 <syhwang4223@gmail.com>
2023-02-02 16:58:29 +0900

    feat : 중복된 이메일, 전화번호 확인을 인증코드 발송 전이 아닌 인증코드 검증 과정에서 진행하도록 수정

--- src/main/java/traveler/travel/domain/account/controller/UserApiController.java
+++ src/main/java/traveler/travel/domain/account/controller/UserApiController.java
@@ -2,6 +2,7 @@ package traveler.travel.domain.account.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import traveler.travel.domain.account.Login;
@@ -25,7 +26,6 @@ import java.util.Optional;
 public class UserApiController {
     private final UserService userService;
     private final MailService mailService;
-    private final RedisUtil redisUtil;
     private final SmsService smsService;
 
     private final UserRepository userRepository;
@@ -57,42 +57,38 @@ public class UserApiController {
 
     // 이메일, 전화번호 인증 발송
     @PostMapping("/signup/authcode")
-    public ResponseDto sendAuthCode(@RequestBody EmailAndPhoneAuthDto authDto) throws Exception {
-        if (authDto.getType().equals(EmailAndPhoneAuthDto.Type.EMAIL)) {
-            // 이메일 중복 확인
-            if (userService.checkEmailDuplicate(authDto.getEmail())) {
-                // 이미 가입된 이메일 주소
-                return new ResponseDto<>(null);
-            } else {
-                // 인증 메일 발송
-                String code = mailService.sendAuthMail(authDto.getEmail());
-                return new ResponseDto(null);
-            }
-        } else if (authDto.getType().equals(EmailAndPhoneAuthDto.Type.PHONE)) {
-            // 전화번호 중복 확인
-            if (userService.checkPhoneNumDuplicate(authDto.getPhoneNum())) {
-                return new ResponseDto<>(null);
-            } else {
-                // 인증 문자 발송
-                SmsService.SmsResponseDto response = smsService.sendAuthCode(authDto.getPhoneNum());
-                return new ResponseDto(response);
-            }
+    public ResponseEntity sendAuthCode(@RequestBody EmailAndPhoneAuthDto authDto) throws Exception {
 
+        if (authDto.getEmail() != null) {
+
+            // 인증 메일 발송
+            mailService.sendAuthMail(authDto.getEmail());
+            return new ResponseEntity(HttpStatus.OK);
+
+        } else if (authDto.getPhoneNum() != null) {
+
+            // 인증 문자 발송
+            smsService.sendAuthCode(authDto.getPhoneNum());
+            return new ResponseEntity(HttpStatus.OK);
+
+        } else {
+            return new ResponseEntity(HttpStatus.BAD_REQUEST);
         }
-        return new ResponseDto<>(null);
     }
 
     // 인증
     @PostMapping("/signup/authcode/validate")
-    public ResponseDto<String> validateAuthCode(@RequestBody EmailAndPhoneAuthDto authDto) {
-        String value = authDto.getType().equals(EmailAndPhoneAuthDto.Type.EMAIL)? redisUtil.getValue(authDto.getEmail()) : redisUtil.getValue(authDto.getPhoneNum());
-        if (value == null || !value.equals(authDto.getCode())) {
-            // 유효하지 않은 인증번호
-            return new ResponseDto<String>("invalid code");
+    public ResponseEntity<String> validateAuthCode(@RequestBody EmailAndPhoneAuthDto authDto) {
+
+        String result = userService.checkAuthCode(authDto);
+
+        if (result.equals("200")) {
+            return new ResponseEntity<>(HttpStatus.OK);
+
         } else {
-            // 인증 성공
-            return new ResponseDto<String>("success");
+            return new ResponseEntity<>(result, HttpStatus.BAD_REQUEST);
         }
+
     }
 
     //회원 수정
